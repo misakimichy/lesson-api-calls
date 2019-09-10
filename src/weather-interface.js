1386254dd374cf72684a28bfcc498b53423b632f
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-09 22:05:37 -0700

    Import WeatherService class from backend logic.

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,34 +1,24 @@
+import $ from 'jquery';
+import { WeatherService } from "./weather-service";
+
 // UI LOGIC BEGIN
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
-// UI LOGIC END
 
-// BUSINESS LOGIC BEGIN
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-// BUSINESS LOGIC END
+    // create instance of WeatherService class
+    let weatherService = new WeatherService();
 
-// UI LOGIC BEGIN
+    // call the instance method and pass in user input
+    let promise = weatherService.getWeatherByCity(city);
+
     promise.then(function(response) {
       const body = JSON.parse(response);
       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+    });  
   });
-// UI LOGIC END
 });
